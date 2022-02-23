ed227b2d213bc3d9ddf21f5f52b8fcf6e9268573
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-23 07:00:25 +0530

    Check equality of two lengths

--- src/LineComparisonOOPS/LineComparisonOOPS.java
+++ src/LineComparisonOOPS/LineComparisonOOPS.java
@@ -2,20 +2,35 @@ package LineComparisonOOPS;
 import java.util.Scanner;
 public class LineComparisonOOPS {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Point1's x=");
-		int x1=  sc.nextInt();
-		System.out.println("Enter Point1's y=");
-		int y1=  sc.nextInt();
-		System.out.println("Enter Point2's x=");
-		int x2=  sc.nextInt();			
-        System.out.println("Enter Point2's y=");	
-		int y2=  sc.nextInt();
-	    sc.close();	
-		Double Length =  Math.sqrt(((x2-x1)^2) + ((y2-y1)^2));
-		System.out.printf("Total length of line is", Length );
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+        System.out.print("Enter End point of Line1:");
+        double line1 = LengthCal();
+        System.out.print("Enter End point of Line2:");
+        double line2 = LengthCal();
+        if (line1 == line2) {
+            System.out.print("Lenght is Equal");
+        } else {
+            System.out.print("Lenght is Not Equal");
+        }
+
+    }
+
+    static double LengthCal() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Point1's x=");
+        int x1 = sc.nextInt();
+        System.out.println("Enter Point1's y=");
+        int y1 = sc.nextInt();
+        System.out.println("Enter Point2's x=");
+        int x2 = sc.nextInt();
+        System.out.println("Enter Point2's y=");
+        int y2 = sc.nextInt();
+        sc.close();
+        Double Length = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+        return Length;
+    }
+
+
 
 }
